aac6f80e6a6ae913a445e015151298990c16b1ed
TranDat1306 <datbiggest@example.com>
2023-09-26 00:47:28 +0700

    init project

---
+++ app/src/main/java/com/example/login1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.login1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button tvdk;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvdk = findViewById(R.id.chuyen);
+        tvdk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Chuyen_DK = new Intent(MainActivity.this, Login2.class);
+                startActivity(Chuyen_DK);
+            }
+        });
+    }
+}
